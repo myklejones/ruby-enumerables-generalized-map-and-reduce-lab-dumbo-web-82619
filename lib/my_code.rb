010b83a5d4218f0c9dac5a757f3cabe4242a980f
Mykle jones <mykle.j91@gmail.com>
2019-08-14 21:16:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+    
+def map(array)
+  new = []
+  i=0 
+  while i <array.length 
+  new.push(yield(array[i]))
+  i+=1 
+end
+new 
+end
+
+def reduce (source_array,starting_point= nil)
+   
+  if starting_point
+total = starting_point
+i=0 
+else 
+  total = source_array[0]
+  i=1
+end 
+while i< source_array.length
+total = yield(total,source_array[i])
+i+=1 
+end 
+total 
+end
